ec741517f7daa1c8e3955e71e1d4727a03ff55ad
Christopher Kenny <ctk44@cornell.edu>
2024-01-30 18:10:39 -0500

    Update theme.R

--- R/theme.R
+++ R/theme.R
@@ -45,16 +45,10 @@ theme_alarm_dark <- function(...) {
 #' @rdname theme_alarm
 #' @export
 theme_alarm_light <- function(...) {
-  ggplot2::theme_light() #+
-    # ggplot2::theme(
-    #   plot.background = ggplot2::element_rect(fill = 'black'),
-    #   panel.background = ggplot2::element_rect(fill = '#303030'),
-    #   legend.background = ggplot2::element_rect(fill = '#7F7F7F'),
-    #   text = ggplot2::element_text(color = '#FFFFFF'),
-    #   axis.text = ggplot2::element_text(color = '#FFFFFF'),
-    #   legend.text = ggplot2::element_text(color = 'black'),
-    #   legend.title = ggplot2::element_text(color = 'black'),
-    #   ...
-    # )
+  ggplot2::theme_light() +
+    ggplot2::theme(
+      strip.background = ggplot2::element_rect(fill = '#304160'),
+      ...
+    )
 }
 
