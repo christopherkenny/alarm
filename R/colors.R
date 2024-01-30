3fc28ebdd87d210338ee93a73bd6311ac9ffd43f
Christopher Kenny <ctk44@cornell.edu>
2024-01-30 17:47:04 -0500

    reorder colors, init theme light

--- R/colors.R
+++ R/colors.R
@@ -7,9 +7,9 @@
 #'
 #' @export
 alarm <- list(
-  alarm = structure(c('#905536', '#304160', '#416E55', '#C2C1C2',
-                      '#BD4089', '#E3B505', '#8533A9', '#F4C095',
-                      '#8AC6D0', '#AFA060', '#98DFAF', '#F7B2B7'),
+  alarm = structure(c('#905536', '#304160', '#416E55', '#BD4089',
+                      '#E3B505', '#8533A9', '#F4C095', '#8AC6D0',
+                      '#AFA060', '#98DFAF', '#F7B2B7', '#C2C1C2'),
                       class = c('palette', 'character'))
 )
 
--- R/theme.R
+++ R/theme.R
@@ -1,23 +1,32 @@
-#' ALARM Project theme for [ggplot2]
+#' ALARM Project themes for [ggplot2]
 #'
 #' @param ... arguments to pass to [ggplot2::theme]
 #'
 #' @return a `ggplot2` `theme` element
 #' @export
 #'
+#' @name theme_alarm
+#'
 #' @examples
 #' ggplot2::mpg |>
 #'   ggplot2::ggplot() +
 #'    ggplot2::geom_point(ggplot2::aes(displ, hwy, color = class)) +
 #'    scale_color_alarm() +
-#'    theme_alarm_dark
+#'    theme_alarm_light()
 #'
 #' ggplot2::mpg |>
 #'   ggplot2::ggplot() +
 #'    ggplot2::geom_point(ggplot2::aes(displ, hwy, color = class)) +
 #'    ggplot2::facet_wrap(~manufacturer) +
 #'    scale_color_alarm() +
-#'    theme_alarm_dark
+#'    theme_alarm_light()
+#'
+#' ggplot2::mpg |>
+#'   ggplot2::ggplot() +
+#'    ggplot2::geom_point(ggplot2::aes(displ, hwy, color = class)) +
+#'    ggplot2::facet_wrap(~manufacturer) +
+#'    scale_color_alarm() +
+#'    theme_alarm_dark()
 #'
 theme_alarm_dark <- function(...) {
   ggplot2::theme_dark() +
@@ -32,3 +41,20 @@ theme_alarm_dark <- function(...) {
       ...
     )
 }
+
+#' @rdname theme_alarm
+#' @export
+theme_alarm_light <- function(...) {
+  ggplot2::theme_light() #+
+    # ggplot2::theme(
+    #   plot.background = ggplot2::element_rect(fill = 'black'),
+    #   panel.background = ggplot2::element_rect(fill = '#303030'),
+    #   legend.background = ggplot2::element_rect(fill = '#7F7F7F'),
+    #   text = ggplot2::element_text(color = '#FFFFFF'),
+    #   axis.text = ggplot2::element_text(color = '#FFFFFF'),
+    #   legend.text = ggplot2::element_text(color = 'black'),
+    #   legend.title = ggplot2::element_text(color = 'black'),
+    #   ...
+    # )
+}
+
