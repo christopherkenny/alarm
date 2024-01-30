1e04bdce023be142f6511c7825c82af69dcf2480
Christopher Kenny <ctk44@cornell.edu>
2024-01-30 17:06:52 -0500

    themes

--- R/theme.R
+++ R/theme.R
@@ -10,16 +10,25 @@
 #'   ggplot2::ggplot() +
 #'    ggplot2::geom_point(ggplot2::aes(displ, hwy, color = class)) +
 #'    scale_color_alarm() +
-#'    theme_alarm_dark()
+#'    theme_alarm_dark
+#'
+#' ggplot2::mpg |>
+#'   ggplot2::ggplot() +
+#'    ggplot2::geom_point(ggplot2::aes(displ, hwy, color = class)) +
+#'    ggplot2::facet_wrap(~manufacturer) +
+#'    scale_color_alarm() +
+#'    theme_alarm_dark
+#'
 theme_alarm_dark <- function(...) {
   ggplot2::theme_dark() +
     ggplot2::theme(
-      plot.background = ggplot2::element_rect(fill = '#303030'),
-      panel.background = ggplot2::element_rect(fill = 'grey40'),
+      plot.background = ggplot2::element_rect(fill = 'black'),
+      panel.background = ggplot2::element_rect(fill = '#303030'),
+      legend.background = ggplot2::element_rect(fill = '#7F7F7F'),
       text = ggplot2::element_text(color = '#FFFFFF'),
       axis.text = ggplot2::element_text(color = '#FFFFFF'),
-      legend.text = ggplot2::element_text(color = '#303030'),
-      legend.title = ggplot2::element_text(color = '#303030'),
+      legend.text = ggplot2::element_text(color = 'black'),
+      legend.title = ggplot2::element_text(color = 'black'),
       ...
     )
 }
