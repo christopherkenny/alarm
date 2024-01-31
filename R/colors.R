3003f401efa9c48dbc6ab3c838b4bc9371049ecb
Christopher Kenny <ctk44@cornell.edu>
2024-01-30 22:15:10 -0500

    add colors changes

--- R/colors.R
+++ R/colors.R
@@ -9,7 +9,7 @@
 alarm <- list(
   alarm = structure(c('#905536', '#304160', '#416E55', '#BD4089',
                       '#E3B505', '#8533A9', '#F4C095', '#8AC6D0',
-                      '#AFA060', '#98DFAF', '#F7B2B7', '#C2C1C2'),
+                      '#918551', '#77E0B0', '#F7B3BB', '#C2C1C2'),
                       class = c('palette', 'character'))
 )
 
